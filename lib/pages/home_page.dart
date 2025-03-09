3fecb9b782280724b8de0c4c9fc6304006adb48f
MrPuffyWuff <128201575+MrPuffyWuff@users.noreply.github.com>
2025-03-08 20:25:21 -0500

    Popup
    VideoWidget (kindof) working
     - Put into its own file
    Home Page contains one popup

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:hacktj_25_front_end/components/video_widget.dart';
 import 'package:video_player/video_player.dart';
 
 
@@ -16,77 +18,59 @@ class _HomePageState extends State<HomePage> {
       appBar: AppBar(title: const Text('Home Page')),
       // Use a FutureBuilder to display a loading spinner while waiting for the
       // VideoPlayerController to finish initializing.
-      body: Row(
-        children: [
-          VideoWidget(),
-          VideoWidget(),
-          VideoWidget(),
-        ],
+      body: SizedBox(
+        height:50,
+        child: Row(
+          children: [
+            Popup(
+              videoPath: 'https://flutter.github.io/assets-for-api-docs/assets/videos/butterfly.mp4',
+              header: 'EVENT: SOMETHING HAPPENED',
+              description: 'Cool this should ACTIVATE.',
+            ),
+          ],
+        ),
       ),
     );
     return test;
   }
 }
 
-class VideoWidget extends StatelessWidget {
-  late final VideoPlayerController _controller = VideoPlayerController.networkUrl(
-    Uri.parse(
-      'https://flutter.github.io/assets-for-api-docs/assets/videos/butterfly.mp4',
-      //'C:\\Users\\jaspe.MSI\\Documents\\GithubLocal\\hacktj_25_front_end\\debug_video.mp4',
-    ),
-  );
-  late final Future<void> _initializeVideoPlayerFuture = _controller.initialize();
-  //_controller.setLooping(true);
-  @override
-  void dispose() {
-    // Ensure disposing of the VideoPlayerController to free up resources.
-    _controller.dispose();
-  }
+class Popup extends StatelessWidget {
 
-  VideoWidget({
+  final String videoPath;
+  final String header;
+  final String description;
+
+  const Popup({
     super.key,
+    required this.videoPath,
+    required this.header,
+    required this.description
   });
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        FutureBuilder(
-          future: _initializeVideoPlayerFuture,
-          builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.done) {
-              // If the VideoPlayerController has finished initialization, use
-              // the data it provides to limit the aspect ratio of the video.
-              return SizedBox(
-                width:100,
-                // Use the VideoPlayer widget to display the video.
-                child: VideoPlayer(_controller),
-              );
-            } else {
-              // If the VideoPlayerController is still initializing, show a
-              // loading spinner.
-              return const Center(child: CircularProgressIndicator());
-            }
-          },
+    return Container(
+      height:200,
+      decoration: BoxDecoration(
+        border: Border.all(
+          color: Colors.indigo,
+          width: 5.0,
         ),
-        ElevatedButton(
-          onPressed: () {
-            // Wrap the play or pause in a call to `setState`. This ensures the
-            // correct icon is shown.
-            // If the video is playing, pause it.
-            if (_controller.value.isPlaying) {
-              _controller.pause();
-            } else {
-              // If the video is paused, play it.
-              _controller.play();
-            }
-          },
-          // Display the correct icon depending on the state of the player.
-          child: Icon(
-            _controller.value.isPlaying ? Icons.pause : Icons.play_arrow,
+        borderRadius: BorderRadius.circular(10.0),
+      ),
+      child: Row(
+        children: [
+          VideoWidget(path: videoPath),
+          Column(
+            children: [
+              Text(header),
+              Text(description),
+            ],
           ),
-        ),
-      ],
+        ],
+      )
     );
   }
 }
+
